d61483cff0c957ad84d35617e8263725f304ce26
EduardoFujiki <eduardofmaeda@hotmail.com>
2023-08-22 20:30:55 -0300

    refactor UDP Server

--- src/Client.java
+++ src/Client.java
@@ -1,38 +1,39 @@
-import java.io.*;
-import java.net.*;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 public class Client {
     public static void main(String[] args) {
-        String serverAddress = "localhost"; // Change this to the server's address
-        int serverPort = 12345; // Change this to the server's port
+        String serverAddress = "localhost"; // Endereço do servidor
+        int serverPort = 12345; // Porta do servidor
 
-        try (Socket socket = new Socket(serverAddress, serverPort);
-             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-
-            System.out.println("Connected to server.");
-            System.out.println("Enter text to send to the server. Type 'exit' to quit.");
+        try (DatagramSocket socket = new DatagramSocket()) {
+            System.out.println("Conectado ao servidor.");
 
             int count = 600;
             String userInput;
-            for(int i = 0; i<count; i++){
+            InetAddress serverInetAddress = InetAddress.getByName(serverAddress);
+
+            for (int i = 0; i < count; i++) {
                 userInput = "Numero:" + i;
                 count++;
                 System.out.println(userInput);
                 if (userInput.equals("Numero:600")) {
                     break;
                 }
-                writer.write(userInput);
-                writer.newLine();
-                writer.flush();
+
+                byte[] sendData = userInput.getBytes();
+                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverInetAddress, serverPort);
+                socket.send(sendPacket);
+
                 try {
-                    Thread.sleep(1000); // Espera 1 segundo entre cada mensagem
+                    Thread.sleep(1000); //wait 1 sec for each message
                 } catch (InterruptedException e) {
                     Thread.currentThread().interrupt();
                     return;
                 }
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/Server.java
+++ src/Server.java
@@ -1,24 +1,38 @@
 import java.io.*;
-import java.net.*;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Server {
-    private static int clientCounter = 0;
+    private static Map<String, Integer> clientIds = new HashMap<>();
+    private static int nextClientId = 1;
 
     public static void main(String[] args) {
-        int portNumber = 12345; // Change this to your desired port number
-        ExecutorService executorService = Executors.newFixedThreadPool(5); // Allow up to 5 clients
+        int portNumber = 12345; // Port number for the UDP server
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
 
-        try (ServerSocket serverSocket = new ServerSocket(portNumber)) {
+        try (DatagramSocket serverSocket = new DatagramSocket(portNumber)) {
             System.out.println("Server started");
 
             while (true) {
-                Socket clientSocket = serverSocket.accept();
-                System.out.println("New client connected: " + clientSocket.getInetAddress().getHostAddress());
+                byte[] receiveData = new byte[1024];
+                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
 
-                // Create a new thread for each client
-                ClientHandler clientHandler = new ClientHandler(clientSocket);
+                serverSocket.receive(receivePacket);
+                String clientAddress = receivePacket.getAddress().getHostAddress();
+                int clientPort = receivePacket.getPort();
+                String clientKey = clientAddress + ":" + clientPort;
+
+                // Get the client's ID or assign a new one based on the client's IP and port
+                int clientId = getClientId(clientKey);
+
+                System.out.println("Client (ID: " + clientId + ") connected from: " + clientKey);
+
+                // Create a new thread for each client with their unique ID
+                ClientHandler clientHandler = new ClientHandler(serverSocket, receivePacket, clientId);
                 executorService.execute(clientHandler);
             }
         } catch (IOException e) {
@@ -26,40 +40,35 @@ public class Server {
         }
     }
 
-    public static synchronized int getNextClientId() {
-        return ++clientCounter;
+    public static synchronized int getClientId(String clientKey) {
+        return clientIds.computeIfAbsent(clientKey, k -> nextClientId++);
     }
 }
 
 class ClientHandler implements Runnable {
-    private Socket clientSocket;
+    private DatagramSocket serverSocket;
+    private DatagramPacket receivePacket;
     private int clientId;
 
-    public ClientHandler(Socket clientSocket) {
-        this.clientSocket = clientSocket;
-        this.clientId = Server.getNextClientId();
+    public ClientHandler(DatagramSocket serverSocket, DatagramPacket receivePacket, int clientId) {
+        this.serverSocket = serverSocket;
+        this.receivePacket = receivePacket;
+        this.clientId = clientId;
     }
 
     @Override
     public void run() {
         try {
-            // Create a unique file for each client
-            String clientFileName = "client_" + clientId + ".txt";
-            BufferedWriter writer = new BufferedWriter(new FileWriter(clientFileName));
-
-            BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            String inputLine;
-
-            while ((inputLine = reader.readLine()) != null) {
-                System.out.println("Received from client " + clientId + ": " + inputLine);
-                writer.write(inputLine);
+            // Criar um arquivo para cada cliente e armazenar mensagens nele
+            String fileName = "client_" + clientId + ".txt";
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true))) {
+                String message = new String(receivePacket.getData(), 0, receivePacket.getLength());
+                writer.write(message);
                 writer.newLine();
-                writer.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-
-            writer.close();
-            clientSocket.close();
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
